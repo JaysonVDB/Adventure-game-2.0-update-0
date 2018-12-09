390c80da881efd47b688207c6ac8cda247457b23
Jayson <Jaysonvanderberg@gmail.com>
2018-12-09 23:50:58 +0100

    update 1  .... 1-12-2018

---
+++ adventure.js
@@ -0,0 +1,24 @@
+			//titel shit enzo var weet niet zalwel
+
+
+			var titlew1 = document.getElementById("title")
+			titlew1.innerHTML = "hallo +;-"
+
+			//allemaal onzin / var enzo voor in die button dingen ofzo
+
+			var buttonw1 = document.getElementById("button1")
+			buttonw1.innerHTML = "verder"
+			
+		////////////////
+
+
+			var achtergrond = document.createElement("img");
+			achtergrond.setAttribute("background-color", "black");
+			document.getElementById("game-container").appendChild(achtergrond);
+
+
+			achtergrond.setAttribute("src", 'roabin.jpg')
+
+			buttonw1.style.top = "60%"
+			buttonw2.style.top = "60%"
+			buttonw3.style.top = "60%"
